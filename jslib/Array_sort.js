3993ca4d8ac7747f45c6b61d3eb56b6f3b9aebda
oishi <oishi@pl.info.kochi-tech.ac.jp>
2016-09-30 18:33:39 +0900

    Fix the Array_sort

--- jslib/Array_sort.js
+++ jslib/Array_sort.js
@@ -1,7 +1,7 @@
-Array.prototype.sort = function(compf) {
+Array.prototype.sort2 = function(compf) {
 	var element = this;
 	if (compf === undefined) {
-	  compf = function(a, b){
+	    compf = function(a, b) {
 	    a = a.toString();
 	    b = b.toString();
 	    if (a < b) return -1;
@@ -11,15 +11,26 @@ Array.prototype.sort = function(compf) {
 	}
 	for (var i=0;i<element.length-1;i++) {
 	  for (var j=element.length-1;j>i;j--) {	
-	    if (compf(element[j-1], element[j]) > 0) {
+	    	 if (element[j-1] === undefined && element[j] != undefined) {
 	      var swap = element[j];
 	      element[j] = element[j-1];
 	      element[j-1] = swap;
 	    }
+	    else if (element[j-1] != undefined  && element[j] != undefined) {
+	    	if (compf(element[j-1], element[j]) > 0) {
+	    		var swap = element[j];
+	     	 	element[j] = element[j-1];
+	      		element[j-1] = swap;
+	    	}
+	 		}
 	  }
 	}
 	return element;
 };
 
-//var a = [3, 5, 3, 1, 8];
-
+var a = [];
+a[8] = 1;
+a[5] = 5 ;
+a[10] = 11;
+var as = a.sort2();
+console.log(as);
