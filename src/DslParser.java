246e2272916adf325e508217deb492b649ee0736
gyane <gyanexus7@gmail.com>
2017-05-31 17:03:55 +0900

    prologue/epilogue add inst

--- src/DslParser.java
+++ src/DslParser.java
@@ -397,9 +397,6 @@ public class DslParser {
     public InstDef run(String fname) {
         String all = readAll(fname);
         List<Token> tks = new Tokenizer().tokenize(all);
-        for (Token t : tks) {
-            System.out.println(t);
-        }
         InstDef instDef = parse(tks);
         return instDef;
     }
