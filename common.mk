6b4b074b59e7ff94a5c27e5ffcc54d582c2433a9
UGAWA Tomoharu <ugawa@spa.info.kochi-tech.ac.jp>
2017-06-17 14:25:17 +0900

    new build system

---
+++ common.mk
@@ -0,0 +1,223 @@
+## Uncomment after editing options and paths
+#
+#all: ejsvm
+#
+
+CC = clang
+
+###
+### Options
+###
+
+# GC=native|boehmgc|none
+OPT_GC=native
+
+# REGEXP=oniguruma|none
+OPT_REGEXP=oniguruma
+
+###
+### Paths
+###
+
+INCLUDES=-I/usr/local/include
+LIBS=-L/usr/local/lib
+
+CFLAGS += -std=gnu89 -Wall -Wno-unused-label -Wno-format -DUSER_DEF $(INCLUDES)
+LIBS += -lm
+
+
+ifeq ($(OPT_GC),native)
+	CFLAGS+=-DUSE_NATIVEGC=1
+endif
+ifeq ($(OPT_GC),boehmgc)
+	CFLAGS+=-DUSE_BOEHMGC=1
+	LIBS+=-lgc
+endif
+ifeq ($(OPT_REGEXP),oniguruma)
+	CFLAGS+=-DUSE_REGEXP=1
+	LIBS+=-lonig
+endif
+
+GENERATED_HFILES = \
+         instructions-opcode.h \
+	 instructions-table.h \
+	 instructions-label.h \
+	 cell-header.h
+
+HFILES = $(GENERATED_HFILES) \
+         prefix.h \
+         context.h \
+         header.h \
+	 builtin.h \
+	 hash.h \
+	 instructions.h \
+	 types.h \
+	 globals.h \
+	 extern.h \
+	 log.h \
+	 gc.h
+
+OFILES = allocate.o \
+         builtin-array.o \
+         builtin-boolean.o \
+         builtin-global.o \
+         builtin-math.o \
+         builtin-number.o \
+         builtin-object.o \
+         builtin-regexp.o \
+         builtin-string.o \
+         call.o \
+         codeloader.o \
+         context.o \
+         conversion.o \
+         hash.o \
+         init.o \
+	 string.o \
+         object.o \
+         operations.o \
+         vmloop.o \
+         gc.o \
+         main.o
+
+INSN_GENERATED = \
+	insns/add.inc \
+	insns/bitand.inc \
+	insns/bitor.inc \
+	insns/call.inc \
+	insns/div.inc \
+	insns/eq.inc \
+	insns/equal.inc \
+	insns/getprop.inc \
+	insns/leftshift.inc \
+	insns/lessthan.inc \
+	insns/lessthanequal.inc \
+	insns/mod.inc \
+	insns/mul.inc \
+	insns/new.inc \
+	insns/rightshift.inc \
+	insns/setprop.inc \
+	insns/sub.inc \
+	insns/tailcall.inc \
+	insns/unsignedrightshift.inc
+
+INSN_HANDCRAFT = \
+         insns/end.inc \
+         insns/error.inc \
+         insns/fixnum.inc \
+         insns/geta.inc \
+         insns/getarg.inc \
+         insns/geterr.inc \
+         insns/getglobal.inc \
+         insns/getglobalobj.inc \
+         insns/getidx.inc \
+         insns/getlocal.inc \
+         insns/instanceof.inc \
+         insns/isobject.inc \
+         insns/isundef.inc \
+         insns/jump.inc \
+         insns/jumpfalse.inc \
+         insns/jumptrue.inc \
+         insns/localcall.inc \
+         insns/localret.inc \
+         insns/makeclosure.inc \
+         insns/makeiterator.inc \
+         insns/move.inc \
+         insns/newargs.inc \
+         insns/nextpropname.inc \
+         insns/nop.inc \
+         insns/not.inc \
+         insns/number.inc \
+         insns/pophandler.inc \
+         insns/poplocal.inc \
+         insns/pushhandler.inc \
+         insns/ret.inc \
+         insns/seta.inc \
+         insns/setarg.inc \
+         insns/setarray.inc \
+         insns/setfl.inc \
+         insns/setglobal.inc \
+         insns/setlocal.inc \
+         insns/specconst.inc \
+         insns/throw.inc \
+         insns/typeof.inc \
+         insns/unknown.inc
+
+ifeq ($(DATATYPES),)
+else
+	CFLAGS += -DUSE_TYPES_GENERATED=1
+	GENERATED_HFILES += types-generated.h
+endif
+
+INSN_FILES = $(INSN_GENERATED) $(INSN_HANDCRAFT)
+
+SEDCOM_GEN_INSN_OPCODE = \
+  -e 's/^\([a-z][a-z]*\).*/\U\1,/' -e '/^\/\/.*/d'
+SEDCOM_GEN_INSN_TABLE = \
+  -e 's/^\([a-z][a-z]*\)  *\([A-Z][A-Z]*\).*/  { "\1", \2 },/' -e '/^\/\/.*/d'
+SEDCOM_GEN_INSN_LABEL = \
+  -e 's/^\([a-z][a-z]*\).*/\&\&I_\U\1,/' -e '/^\/\/.*/d'
+SED = gsed
+RUBY = ruby
+
+message:
+	@echo "This is a template of Makefile.  Copy Makefile to your own"
+	@echo "Makefile such as Makefile.mine, edit it, and make with your"
+	@echo "own Makefile."
+	@echo "   make -f Makefile.mine"
+
+ejsvm-with-suffix: ejsvm$(SUFFIX)
+
+ejsvm :: $(OFILES)
+	$(CC) -o $@ $^ $(LIBS)
+
+instructions-opcode.h: ../instructions.def
+	$(SED) $(SEDCOM_GEN_INSN_OPCODE) ../instructions.def > $@
+
+instructions-table.h: ../instructions.def
+	$(SED) $(SEDCOM_GEN_INSN_TABLE) ../instructions.def > $@
+
+instructions-label.h: ../instructions.def
+	$(SED) $(SEDCOM_GEN_INSN_LABEL) ../instructions.def > $@
+
+instructions.h: instructions-opcode.h instructions-table.h
+
+cell-header.h: ../cell-header.def
+	$(RUBY) $< > $@
+
+vmloop-cases.inc: ../instructions.def ../gen-vmloop-cases.rb
+	$(RUBY) ../gen-vmloop-cases.rb < ../instructions.def > $@
+
+
+ifeq ($(DATATYPES),)
+insns/%.inc: ../insns-handcraft/%.inc
+	mkdir -p insns
+	cp $< $@
+else
+$(INSN_GENERATED):insns/%.inc: ../insns-def/%.idef
+	mkdir -p insns
+	java -cp ../vmgen/vmgen.jar InsnGen $(DATATYPES) $< insns
+
+$(INSN_HANDCRAFT):insns/%.inc: ../insns-handcraft/%.inc
+	mkdir -p insns
+	cp $< $@
+endif
+
+%.c: ../%.c
+	cp $< $@
+
+%.h: ../%.h
+	cp $< $@
+
+vmloop.o: vmloop.c vmloop-cases.inc $(INSN_FILES) $(HFILES)
+	$(CC) -c $(CFLAGS) -o $@ $<
+
+%.o: %.c $(HFILES)
+	$(CC) -c $(CFLAGS) -o $@ $<
+
+types-generated.h: $(DATATYPES)
+	java -cp ../vmgen/vmgen.jar TypesGen $< > $@
+
+clean:
+	rm -f *.o $(GENERATED_HFILES) vmloop-cases.inc
+	rm -rf insns
+
