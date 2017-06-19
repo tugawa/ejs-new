a9a4b91bf8686159caea5fbc26327b26759f9c3b
UGAWA Tomoharu <ugawa@spa.info.kochi-tech.ac.jp>
2017-06-20 00:27:06 +0900

    Modified simple synthsiser so that it uses predicats

--- src/SimpleSynthesiser.java
+++ src/SimpleSynthesiser.java
@@ -1,5 +1,6 @@
 import java.io.FileNotFoundException;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class SimpleSynthesiser extends Synthesiser {
 	@Override
@@ -9,43 +10,23 @@ public class SimpleSynthesiser extends Synthesiser {
 		StringBuilder code = new StringBuilder();
 
 		for (Plan.Rule r: rules) {
-			String cstr = r.condition.stream()
-			.map(c -> SynthesiseCondition(plan.dispatchVars, c, plan.getArity(), 0, ""))
-			.reduce((acc, term) -> {
-				if (acc == null)
-					return term;
-				else
-					return acc + " || " + term;
-			}).get();
-
-			code.append("if (" + cstr + ") {\n");
+			code.append("if (");
+			code.append(r.condition.stream()
+							.map(c -> {
+								String s = "(";
+								for (int i = 0; i < plan.dispatchVars.length; i++) {
+									if (i > 0) s += " && ";
+									s += "is_"+c.dts[i].name+"("+plan.dispatchVars[i]+")";
+								}
+								return s + ")";
+							}).collect(Collectors.joining(" || ")));
+			code.append(") {\n");
 			code.append(r.action + "\n");
 			code.append("} else \n");
 		}
 		code.append("{}");
 
 		return code.toString();
-	}
-
-	String SynthesiseCondition(String[] dispatchVars, Plan.Condition c, int n, int i, String part) {
-		if (i == n)
-			return "(" + part + ")";
-
-		return c.dts[i].getRepresentations().stream().map(tr -> {
-			String s = part;
-			if (i > 0)
-				s += " && ";
-			s += getPTCode(dispatchVars[i]) + " == " + tr.getPT().name;
-			if (tr.hasHT())
-				s += " && " + getHTCode(dispatchVars[i]) + " == " + tr.getHT().name;
-			return SynthesiseCondition(dispatchVars, c, n, i + 1, s);
-		})
-		.reduce((acc, term) -> {
-			if (acc == null)
-				return term;
-			else
-				return acc + " || " + term;
-		}).get();
 	}
 
 	public static void main(String[] args) throws FileNotFoundException {
