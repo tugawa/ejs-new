aa6ac3007631c650c5c664aa63f5fef23c03a325
gyaneman <gyanexus7@gmail.com>
2017-12-08 19:53:52 +0900

    small change

--- context.c
+++ context.c
@@ -33,11 +33,11 @@ FunctionFrame *new_frame(Context *ctx, FunctionTable *ft, FunctionFrame *env, in
   int i;
 
   nl++;   /* GC_DEBUG (canary; search GC_DEBUG in gc.c) */
-  gc_push_tmp_root_malloc(&env);
+  gc_push_tmp_root(&env);
   frame = (FunctionFrame *)
     gc_malloc(ctx, sizeof(FunctionFrame) + BYTES_IN_JSVALUE * nl,
 		       HTAG_FUNCTION_FRAME);
-  gc_pop_tmp_root_malloc(1);
+  gc_pop_tmp_root(1);
   frame->prev_frame = env;
   locals = frame->locals;
   for (i = 0; i < nl; i++)
