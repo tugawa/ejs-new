800ed61cd9db609c66689a68799dfca333c728fa
Tomoharu Ugawa <ugawa.tomoharu@kochi-tech.ac.jp>
2018-12-27 18:53:45 +0900

    support complex dispatch conditions

--- test/TestVMDLC.java
+++ test/TestVMDLC.java
@@ -7,15 +7,16 @@ import nez.parser.ParserStrategy;
 //import nez.parser.io.StringSource;
 import nez.parser.io.FileSource;
 import nez.ast.Source;
+import nez.ast.SourceError;
 import nez.util.ConsoleUtils;
 
-import vmdlc.DesugarVisitor;
-import vmdlc.AlphaConvVisitor;
-import vmdlc.AstToCVisitor;
-import vmdlc.SyntaxTree;
 
 import dispatch.*;
 import type.*;
+import vmdlc.AlphaConvVisitor;
+import vmdlc.AstToCVisitor;
+import vmdlc.DesugarVisitor;
+import vmdlc.SyntaxTree;
 
 public class TestVMDLC {
     public final static void main(String[] args) {
@@ -34,12 +35,18 @@ public class TestVMDLC {
             Parser parser = grammar.newParser(ParserStrategy.newSafeStrategy());
             
             //Source source = new StringSource("externC constant cint aaa = \"-1\";");
-            Source source = new FileSource("vmdl/test2.inc2");
+            Source source = new FileSource("vmdl/test3.inc2");
             SyntaxTree node = (SyntaxTree) parser.parse(source, new SyntaxTree());
+            if (parser.hasErrors()) {
+                for (SourceError e: parser.getErrors()) {
+                    System.out.println(e);
+                }
+            }
 
             new DesugarVisitor().start(node);
             new AlphaConvVisitor().start(node, true);
-            new AstToCVisitor().start(node);
+            String program = new AstToCVisitor().start(node);
+            System.out.println(program);
             
             // ConsoleUtils.println(node);
         } catch (IOException ioe) {
