cc222f4fc383ab067161463ec99f0793a569c169
Tomoharu Ugawa <ugawa.tomoharu@kochi-tech.ac.jp>
2019-01-05 00:54:59 +0900

    add LESSTHAN insn def written in VMDL

--- vmdl-helper.h
+++ vmdl-helper.h
@@ -10,3 +10,10 @@
 #define toObject(v)        to_object(context, (v))
 #define getArrayProp(v1,v2)   get_array_prop(context, (v1), (v2))
 #define getObjectProp(v1,v2)  get_object_prop(context, (v1), (v2))
+#define String_to_cstr(v)  string_to_cstr((v))
+
+
+#define FIXNUM_LESSTHAN(v1,v2) ((int64_t) (v1) < (int64_t) (v2))
+#define Object_to_primitive_hint_number(v) object_to_primitive(context, (v) ,HINT_NUMBER)
+#define Strcmp(x1,x2)     strcmp((x1), (x2))
+
