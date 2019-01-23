21912af4da7e627babd935d90b05ab4984b56d6d
Tomoharu Ugawa <ugawa.tomoharu@kochi-tech.ac.jp>
2019-01-23 13:36:41 +0900

    correct indentation

--- src/vmgen/OperandSpecifications.java
+++ src/vmgen/OperandSpecifications.java
@@ -7,7 +7,7 @@
 
      Tomoharu Ugawa, 2016-18
      Hideya Iwasaki, 2016-18
-*/
+ */
 package vmgen;
 
 import java.io.FileInputStream;
@@ -25,114 +25,114 @@ import java.util.regex.Pattern;
 import vmgen.type.VMDataType;
 
 public class OperandSpecifications {
-	static class OperandSpecificationRecord {
-		String insnName;
-		String[] operandTypes;
-		enum Behaviour {
-			ACCEPT,
-			UNSPECIFIED,
-			ERROR
-		};
-		Behaviour behaviour;
-		OperandSpecificationRecord(String insnName, String[] operandTypes, Behaviour behaviour) {
-			this.insnName = insnName;
-			this.operandTypes = operandTypes;
-			this.behaviour = behaviour;
-		}
-	}
-	List<OperandSpecificationRecord> spec = new ArrayList<OperandSpecificationRecord>();
+    static class OperandSpecificationRecord {
+        String insnName;
+        String[] operandTypes;
+        enum Behaviour {
+            ACCEPT,
+            UNSPECIFIED,
+            ERROR
+        };
+        Behaviour behaviour;
+        OperandSpecificationRecord(String insnName, String[] operandTypes, Behaviour behaviour) {
+            this.insnName = insnName;
+            this.operandTypes = operandTypes;
+            this.behaviour = behaviour;
+        }
+    }
+    List<OperandSpecificationRecord> spec = new ArrayList<OperandSpecificationRecord>();
 
-	void load(Scanner sc) {
-		final String P_SYMBOL = "[a-zA-Z_]+";
-		final String P_OPERANDS = "\\(\\s*([^)]+)\\s*\\)";
-		final String P_BEHAVIOUR = "accept|error|unspecified";
-		final Pattern splitter = Pattern.compile("("+P_SYMBOL+")\\s+"+P_OPERANDS+"\\s+("+P_BEHAVIOUR+")\\s*$");
-		while (sc.hasNextLine()) {
-			String line = sc.nextLine();
-			Matcher matcher = splitter.matcher(line);
-			if (matcher.matches()) {
-				MatchResult m = matcher.toMatchResult();
-				String insnName = m.group(1);
-				String[] allOps = m.group(2).split("\\s*,\\s*");
-				int n = 0;
-				for (String s: allOps)
-					if (!s.equals("-"))
-						n++;
-				String[] operandTypes = new String[n];
-				int i = 0;
-				for (String s: allOps)
-					if (!s.equals("-"))
-						operandTypes[i++] = s;
-				OperandSpecificationRecord.Behaviour behaviour;
-				if (m.group(3).equals("accept"))
-					behaviour = OperandSpecificationRecord.Behaviour.ACCEPT;
-				else if (m.group(3).equals("error"))
-					behaviour = OperandSpecificationRecord.Behaviour.ERROR;
-				else if (m.group(3).equals("unspecified"))
-					behaviour = OperandSpecificationRecord.Behaviour.UNSPECIFIED;
-				else
-					throw new Error("operand specification syntax error:"+ m.group());
-				OperandSpecificationRecord r = new OperandSpecificationRecord(insnName, operandTypes, behaviour);
-				spec.add(r);
-			} else
-				throw new Error("operand specification syntax error:"+ line);
-		}
-	}
-	
-	public void load(String file) throws FileNotFoundException {
-		Scanner sc = new Scanner(new FileInputStream(file));
-		try {
-			load(sc);
-		} finally {
-			sc.close();
-		}
-	}
+    void load(Scanner sc) {
+        final String P_SYMBOL = "[a-zA-Z_]+";
+        final String P_OPERANDS = "\\(\\s*([^)]+)\\s*\\)";
+        final String P_BEHAVIOUR = "accept|error|unspecified";
+        final Pattern splitter = Pattern.compile("("+P_SYMBOL+")\\s+"+P_OPERANDS+"\\s+("+P_BEHAVIOUR+")\\s*$");
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            Matcher matcher = splitter.matcher(line);
+            if (matcher.matches()) {
+                MatchResult m = matcher.toMatchResult();
+                String insnName = m.group(1);
+                String[] allOps = m.group(2).split("\\s*,\\s*");
+                int n = 0;
+                for (String s: allOps)
+                    if (!s.equals("-"))
+                        n++;
+                String[] operandTypes = new String[n];
+                int i = 0;
+                for (String s: allOps)
+                    if (!s.equals("-"))
+                        operandTypes[i++] = s;
+                OperandSpecificationRecord.Behaviour behaviour;
+                if (m.group(3).equals("accept"))
+                    behaviour = OperandSpecificationRecord.Behaviour.ACCEPT;
+                else if (m.group(3).equals("error"))
+                    behaviour = OperandSpecificationRecord.Behaviour.ERROR;
+                else if (m.group(3).equals("unspecified"))
+                    behaviour = OperandSpecificationRecord.Behaviour.UNSPECIFIED;
+                else
+                    throw new Error("operand specification syntax error:"+ m.group());
+                OperandSpecificationRecord r = new OperandSpecificationRecord(insnName, operandTypes, behaviour);
+                spec.add(r);
+            } else
+                throw new Error("operand specification syntax error:"+ line);
+        }
+    }
 
-	boolean matchOperandTypes(String[] specTypes, VMDataType[] types) {
-		if (specTypes.length != types.length)
-			return false;
-		for (int i = 0; i < specTypes.length; i++)
-			if (!specTypes[i].equals("_") &&
-				!(specTypes[i].equals("object") && types[i].isObject()) &&
-				!specTypes[i].equals(types[i].getName()))
-				return false;
-		return true;
-	}
-	
-	OperandSpecificationRecord findSpecificationRecord(String insnName, VMDataType[] types) {
-		for (OperandSpecificationRecord rec: spec) {
-			if (insnName.equals(rec.insnName) &&
-				matchOperandTypes(rec.operandTypes, types))
-				return rec;
-		}
-		throw new Error("unexhaustive type specification for :"+insnName+"("+types[0].getName()+","+types[1].getName()+")");
-	}
-	
-	public Set<VMDataType[]> getOperands(String insnName, int arity, OperandSpecificationRecord.Behaviour behaviour) {
-		Set<VMDataType[]> typess = new HashSet<VMDataType[]>();
-	    int total = 1;
-		for (int i = 0; i < arity; i++)
-			total *= VMDataType.all().size();
-		for (int i = 0; i < total; i++) {
-			VMDataType[] types = new VMDataType[arity];
-			int a = i;
-			for (int j = 0; j < arity; j++) {
-				types[j] = VMDataType.all().get(a % VMDataType.all().size());
-				a /= VMDataType.all().size();
-			}
-			OperandSpecificationRecord rec = findSpecificationRecord(insnName, types);
-			if (rec.behaviour == behaviour)
-				typess.add(types);
-		}
-		return typess;
-	}
-	
-	
-	public Set<VMDataType[]> getUnspecifiedOperands(String insnName, int arity) {
-		return getOperands(insnName, arity, OperandSpecificationRecord.Behaviour.UNSPECIFIED);
-	}
+    public void load(String file) throws FileNotFoundException {
+        Scanner sc = new Scanner(new FileInputStream(file));
+        try {
+            load(sc);
+        } finally {
+            sc.close();
+        }
+    }
 
-	public Set<VMDataType[]> getErrorOperands(String insnName, int arity) {
-		return getOperands(insnName, arity, OperandSpecificationRecord.Behaviour.ERROR);
-	}
+    boolean matchOperandTypes(String[] specTypes, VMDataType[] types) {
+        if (specTypes.length != types.length)
+            return false;
+        for (int i = 0; i < specTypes.length; i++)
+            if (!specTypes[i].equals("_") &&
+                    !(specTypes[i].equals("object") && types[i].isObject()) &&
+                    !specTypes[i].equals(types[i].getName()))
+                return false;
+        return true;
+    }
+
+    OperandSpecificationRecord findSpecificationRecord(String insnName, VMDataType[] types) {
+        for (OperandSpecificationRecord rec: spec) {
+            if (insnName.equals(rec.insnName) &&
+                    matchOperandTypes(rec.operandTypes, types))
+                return rec;
+        }
+        throw new Error("unexhaustive type specification for :"+insnName+"("+types[0].getName()+","+types[1].getName()+")");
+    }
+
+    public Set<VMDataType[]> getOperands(String insnName, int arity, OperandSpecificationRecord.Behaviour behaviour) {
+        Set<VMDataType[]> typess = new HashSet<VMDataType[]>();
+        int total = 1;
+        for (int i = 0; i < arity; i++)
+            total *= VMDataType.all().size();
+        for (int i = 0; i < total; i++) {
+            VMDataType[] types = new VMDataType[arity];
+            int a = i;
+            for (int j = 0; j < arity; j++) {
+                types[j] = VMDataType.all().get(a % VMDataType.all().size());
+                a /= VMDataType.all().size();
+            }
+            OperandSpecificationRecord rec = findSpecificationRecord(insnName, types);
+            if (rec.behaviour == behaviour)
+                typess.add(types);
+        }
+        return typess;
+    }
+
+
+    public Set<VMDataType[]> getUnspecifiedOperands(String insnName, int arity) {
+        return getOperands(insnName, arity, OperandSpecificationRecord.Behaviour.UNSPECIFIED);
+    }
+
+    public Set<VMDataType[]> getErrorOperands(String insnName, int arity) {
+        return getOperands(insnName, arity, OperandSpecificationRecord.Behaviour.ERROR);
+    }
 }
