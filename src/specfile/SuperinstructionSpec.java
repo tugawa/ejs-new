f985903844b95adb33fd373d7e2e8909234a25cf
Yasunao Takano <tyasunao@gmail.com>
2019-08-06 18:45:04 +0900

    copy specgen from vmgen to vmdl

---
+++ src/specfile/SuperinstructionSpec.java
@@ -0,0 +1,95 @@
+package specfile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import specfile.SpecFile.DataType;
+
+public class SuperinstructionSpec {
+    public static class Superinstruction {
+        Superinstruction(String iName, String siName, DataType[] opTypes) {
+            this.iName = iName;
+            this.siName = siName;
+            this.opTypes = opTypes;
+        }
+        private String iName;
+        private String siName;
+        private DataType[] opTypes;
+        public String getBaseName() {
+            return iName;
+        }
+        public String getName() {
+            return siName;
+        }
+        public DataType getOpType(int i) {
+            return opTypes[i];
+        }
+    }
+
+    static final Pattern SISPEC_LINE_PATTERN = Pattern.compile("^\\s*(\\w+)\\((.*)\\)\\s*:\\s*(\\w+)\\s*$");
+    static public SuperinstructionSpec loadFromFile(String fileName) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(fileName));
+        return parse(lines, 0, lines.size());
+    }
+    static public SuperinstructionSpec parse(List<String> lines, int start, int end) {
+        List<Superinstruction> superinstructionSpec = new ArrayList<Superinstruction>();
+        for (int lineNo = start; lineNo < end; lineNo++) {
+            String line = lines.get(lineNo);
+            line = line.replaceFirst("//.*", "");
+            if (line.matches("\\s*"))
+                continue;
+            Matcher m = SISPEC_LINE_PATTERN.matcher(line);
+            if (m.find()) {
+                String iName = m.group(1);
+                String[] operands = m.group(2).split("\\s*,\\s*");
+                String siName = m.group(3);
+                DataType[] opTypes = new DataType[operands.length];
+                for (int i = 0; i < operands.length; i++)
+                    opTypes[i] = SpecFile.dataTypeTable.get(operands[i]);
+                Superinstruction si = new Superinstruction(iName, siName, opTypes);
+                superinstructionSpec.add(si);
+            } else
+                throw new Error("Superinstruction spec parse error: "+lineNo+": "+lines.get(lineNo));
+        }
+        return new SuperinstructionSpec(superinstructionSpec);
+    }
+
+    private SuperinstructionSpec(List<Superinstruction> list) {
+        this.list = list;
+    }
+    private List<Superinstruction> list;
+
+    public String unparse() {
+        StringBuffer sb = new StringBuffer();
+        unparse(sb);
+        return sb.toString();
+    }
+    public void unparse(StringBuffer sb) {
+        for (Superinstruction si: list) {
+            sb.append(si.iName).append("(");
+            for (int i = 0; i < si.opTypes.length; i++) {
+                if (i > 0)
+                    sb.append(",");
+                sb.append(si.opTypes[i].getName());
+            }
+            sb.append("): ").append(si.siName).append("\n");
+        }
+    }
+    public List<Superinstruction> getList() {
+        return list;
+    }
+    public int getOpcodeIndex(String name) {
+        for (int i = 0; i < list.size(); i++)
+            if (list.get(i).siName.equals(name))
+                return i;
+        return -1;
+    }
+    public int numInstructions() {
+        return list.size();
+    }
+}
---
+++ src/vmdlc/SpecFileGen.java
@@ -0,0 +1,45 @@
+package vmdlc;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import specfile.InstructionDef;
+import specfile.SpecFile;
+import specfile.SuperinstructionSpec;
+
+public class SpecFileGen {
+    public static void main(String args[]) throws IOException {
+        SpecFile spec = new SpecFile();
+        String outFileName = null;
+        String fingerprintFileName = null;
+        int i = 0;
+        while (i < args.length) {
+            if (args[i].equals("--insndef")) {
+                String fileName = args[++i];
+                spec.setInstructionDef(InstructionDef.loadFromFile(fileName));
+            } else if (args[i].equals("--sispec")) {
+                String fileName = args[++i];
+                spec.setSuperinstructionSpec(SuperinstructionSpec.loadFromFile(fileName));
+            } else if (args[i].equals("-o"))
+                outFileName = args[++i];
+            else if (args[i].equals("--fingerprint"))
+                fingerprintFileName = args[++i];
+            else
+                throw new Error("Unknown option: "+args[i]);
+            i++;
+        }
+        if (outFileName == null)
+            System.out.print(spec.unparse());
+        else {
+            try (FileOutputStream out = new FileOutputStream(outFileName)) {
+                out.write(spec.unparse().getBytes());
+            }
+        }
+        if (fingerprintFileName != null) {
+            try (FileOutputStream out = new FileOutputStream(fingerprintFileName)) {
+                String f = String.format("0x%02x", spec.getFingerprint());
+                out.write(f.getBytes());
+            }
+        }
+    }
+}
