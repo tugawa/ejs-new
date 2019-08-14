26b593ac1f5f21eb98f4e349c6c4163744cf37ed
Tomoharu Ugawa <ugawa.tomoharu@kochi-tech.ac.jp>
2019-08-14 10:50:25 +0100

    correct indentation

--- vmdl-helper.h
+++ vmdl-helper.h
@@ -1,3 +1,12 @@
+/*
+ * eJS Project
+ * Kochi University of Technology
+ * The University of Electro-communications
+ *
+ * The eJS Project is the successor of the SSJS Project at The University of
+ * Electro-communications.
+ */
+
 #define concat(s1,s2)      ejs_string_concat(context, (s1), (s2))
 #define toString(v)        to_string(context, (v))
 /* #define FlonumToCdouble(f) to_double(context, (f)) */
@@ -52,10 +61,14 @@
 #define Set_pc()        set_pc(context, pc)
 #define Set_sp(n)       set_sp(context, fp - n)
 #define Try_gc()        try_gc(context)
-#define Call_function(fn, n, sendp)        call_function(context, (fn), (n), (sendp))
-#define Call_builtin(fn, n, sendp, newp)   call_builtin(context, (fn), (n), (sendp), (newp))
-#define Tailcall_function(fn, n, sendp)    tailcall_function(context, (fn), (n), (sendp))
-#define Tailcall_builtin(fn, n, sendp)   tailcall_builtin(context, (fn), (n), (sendp), FALSE)
+#define Call_function(fn, n, sendp)		\
+  call_function(context, (fn), (n), (sendp))
+#define Call_builtin(fn, n, sendp, newp)		\
+  call_builtin(context, (fn), (n), (sendp), (newp))
+#define Tailcall_function(fn, n, sendp)			\
+  tailcall_function(context, (fn), (n), (sendp))
+#define Tailcall_builtin(fn, n, sendp)			\
+  tailcall_builtin(context, (fn), (n), (sendp), FALSE)
 #define Update_context()          update_context()
 #define Save_context()            save_context()
 #define New_normal_object()       new_normal_object(context)
@@ -95,41 +108,55 @@
 #define Setglobal(str, src)        setglobal_helper(context, str, src)
 #define Setlocal(link, index, v)   setlocal_helper(context, link, index, v)
 
-#define Pophandler()                int newpc, handler_fp;\
-                                    exhandler_stack_pop(context, &newpc, &handler_fp);
-#define Poplocal()                  int newpc; \
-                                    lcall_stack_pop(context, &newpc);
+#define Pophandler()					\
+  int newpc, handler_fp;				\
+  exhandler_stack_pop(context, &newpc, &handler_fp);
+#define Poplocal()				\
+  int newpc;					\
+  lcall_stack_pop(context, &newpc);
 
-#define Ret()                       if (fp == border) return 1; \
-                                    JSValue* stack = &get_stack(context, 0); \
-                                    restore_special_registers(context, stack, fp - 4);
+#define Ret()							\
+  if (fp == border) return 1;					\
+  JSValue* stack = &get_stack(context, 0);			\
+  restore_special_registers(context, stack, fp - 4);
 
-#define Newframe(frame_len)         FunctionFrame* fr = new_frame(context, get_cf(context), get_lp(context), frame_len); \
-                                    set_lp(context, fr);
+#define Newframe(frame_len)						\
+  FunctionFrame* fr = new_frame(context, get_cf(context),		\
+				get_lp(context), frame_len);		\
+  set_lp(context, fr);
 
-#define Makearguments(make_arguments)   int num_of_args = get_ac(context); \
-                                        save_context(); \
-                                        JSValue args = new_normal_array_with_size(context, num_of_args); \
-                                        update_context(); \
-                                        int i; \
-                                        for (i = 0; i < num_of_args; i++) \
-                                            array_body_index(args, i) = regbase[i + 2]; \
-                                        fframe_arguments(fr) = args; \
-                                        fframe_locals_idx(fr, 0) = args;
+#define Makearguments(make_arguments)					\
+  int num_of_args = get_ac(context);					\
+  save_context();							\
+  JSValue args = new_normal_array_with_size(context, num_of_args);	\
+  update_context();							\
+  int i;								\
+  for (i = 0; i < num_of_args; i++)					\
+    array_body_index(args, i) = regbase[i + 2];				\
+  fframe_arguments(fr) = args;						\
+  fframe_locals_idx(fr, 0) = args;
 
-#define Throw()                         int newpc, handler_fp; \
-                                        exhandler_stack_pop(context, &newpc, &handler_fp); \
-                                        while (handler_fp != fp) { \
-                                            JSValue *stack = &get_stack(context, 0); \
-                                            restore_special_registers(context, stack, fp - 4); \
-                                            set_sp(context, fp - 5); \
-                                            update_context(); \
-                                        } \
-                                        Displacement disp = (Displacement) (newpc - pc); \
-                                        set_pc_relative(disp);
+#define Throw()							\
+  int newpc, handler_fp;					\
+  exhandler_stack_pop(context, &newpc, &handler_fp);		\
+  while (handler_fp != fp) {					\
+    JSValue *stack = &get_stack(context, 0);			\
+    restore_special_registers(context, stack, fp - 4);		\
+    set_sp(context, fp - 5);					\
+    update_context();						\
+  }								\
+  Displacement disp = (Displacement) (newpc - pc);		\
+  set_pc_relative(disp);
 
 
 #define NotImplemented()            NOT_IMPLEMENTED()
 #define Nextpropnameidx(ite)        nextpropnameidx_helper(ite)
 
 #define GOTO(l)                     goto l
+
+
+/* Local Variables:      */
+/* mode: c               */
+/* c-basic-offset: 2     */
+/* indent-tabs-mode: nil */
+/* End:                  */
